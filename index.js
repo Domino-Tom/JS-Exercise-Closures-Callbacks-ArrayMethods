d2aa24dd22e07b38b122ec96dce8acd75140bf3e
Steven Maynard <steventm19@gmail.com>
2020-02-12 11:16:41 -0500

    9 passing

--- index.js
+++ index.js
@@ -179,8 +179,8 @@ function processDuplicateFree(/* CODE HERE ONLY AFTER COMPLETING ALL OTHER TASKS
  * @returns an array with all the runners' full names in the following format: "Smith, John".
  * The full names appear in the array in the same order the runners appear in the `runners` array.
 */
-function getFullNames(/* CODE HERE */) {
-  /* CODE HERE */
+function getFullNames(runners) {
+  return runners.map(item => `${item.last_name}, ${item.first_name}`)
 }
 
 /**
